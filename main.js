8e75078cdf2f1f7de99426be606614f9cfc472ea
AE <austence@sbcglobal.net>
2019-03-12 14:21:12 -0700

    adds blank lines for re-push in main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // Write your JavaScript here
+
+
 function handleClickEvent (e) {
     var amountDue = document.getElementById ("amount-due").value;
     var amountReceived = document.getElementById ("amount-received").value;
