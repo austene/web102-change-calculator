e9767618618d56d52e747caf34392246d2e1f273
AE <austence@sbcglobal.net>
2019-03-12 13:51:18 -0700

    deletes blank spaces from main.js

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 // Write your JavaScript here
-
-
-
-
-
-
 function handleClickEvent (e) {
     var amountDue = document.getElementById ("amount-due").value;
     var amountReceived = document.getElementById ("amount-received").value;
